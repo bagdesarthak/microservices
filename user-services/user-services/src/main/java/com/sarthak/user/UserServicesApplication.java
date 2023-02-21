504fba76bd7c4c135f2b8a33d848bbe1c08cecbb
bagdesarthak <105451049+bagdesarthak@users.noreply.github.com>
2023-02-21 21:11:09 +0530

    Add files via upload

---
+++ user-services/user-services/src/main/java/com/sarthak/user/UserServicesApplication.java
@@ -0,0 +1,20 @@
+package com.sarthak.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicesApplication.class, args);
+	}
+
+		@Bean
+		public RestTemplate restTemplate(){
+
+			return new RestTemplate();
+		}
+}
